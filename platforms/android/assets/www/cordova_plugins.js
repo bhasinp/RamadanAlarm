38f96c9f8a9d869d6ef7f0b8558f0a394bf750a5
bhasinp <bhasinp@gmail.com>
2015-05-31 21:19:19 -0400

    icons
    icons

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -49,6 +49,7 @@ module.exports.metadata =
     "nl.x-services.plugins.toast": "2.0.3",
     "de.appplant.cordova.plugin.local-notification": "0.8.2dev",
     "org.apache.cordova.geolocation": "0.3.12",
+    "de.appplant.cordova.common.registerusernotificationsettings": "1.0.1",
     "org.apache.cordova.device": "0.3.0",
     "android.support.v4": "21.0.1"
 }
